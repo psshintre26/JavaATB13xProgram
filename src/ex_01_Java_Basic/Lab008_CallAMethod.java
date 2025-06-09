0b10b64f681d1ee4a72a4fce93d5d58b937c55b0
psshintre26 <shintrespallavi@gmail.com>
2025-06-09 11:48:05 +0530

    added new class for method

---
+++ src/ex_01_Java_Basic/Lab008_CallAMethod.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab008_CallAMethod {
+    static void MyMethod(){
+        System.out.println("This is my method written");
+
+    }
+
+    public static void main(String[] args) {
+        MyMethod();
+        System.out.println("My method is ran");
+    }
+}
