e562af310e45c726825afdc0795f8aa93bade490
psshintre26 <shintrespallavi@gmail.com>
2025-06-09 10:53:20 +0530

    code change line 5,6

--- src/ex_01_Java_Basic/Lab007_keywords.java
+++ src/ex_01_Java_Basic/Lab007_keywords.java
@@ -1,4 +1,8 @@
 package ex_01_Java_Basic;
 
 public class Lab007_keywords {
+    public static void main(String[] args) {
+        System.out.println("pushing first change to git");
+        System.out.println("build1");
+    }
 }
