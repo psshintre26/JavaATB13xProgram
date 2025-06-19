4bc22b966137776325af168ed52f4f146bbad387
psshintre26 <shintrespallavi@gmail.com>
2025-06-19 08:27:31 +0530

    June19 coding test

---
+++ src/Test1/Challenge6.java
@@ -0,0 +1,28 @@
+package Test1;
+
+import java.util.Scanner;
+
+public class Challenge6 {
+//    Number Classification
+//    Write a program to check if a number is positive, negative, or zero.
+//
+//**Requirements:**
+//            - Read an integer from user input
+//- Use if-else statements to classify the number
+//- Print appropriate message for each case
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("enter number");
+    int inum = scanner.nextInt();
+    if (inum>0){
+        System.out.println("positive "+inum);
+    } else if (inum==0) {
+        System.out.println("zero "+inum);
+
+    }else{
+        System.out.println("negative "+inum);
+    }
+
+}
+}
--- src/Test1/CodingChallenge4.java
+++ src/Test1/CodingChallenge4.java
@@ -1,4 +1,19 @@
 package Test1;
 
 public class CodingChallenge4 {
+    public static void main(String[] args) {
+//        Create a program that demonstrates pre-increment, post-increment, pre-decrement, and post-decrement operators.
+//
+//                **Requirements:**
+//        - Show the difference between ++i and i++
+//                - Show the difference between --i and i--
+//                - Print values before and after operations
+//        int i = 5;
+//        System.out.println("Pre-increment: "+++i);
+//        System.out.println("Post-increment: "+ (i++));
+//        System.out.println("Pre-decrement:  "+ (--i));
+//        System.out.println("Post-decrement:  "+ (i--));
+
+
+    }
 }
